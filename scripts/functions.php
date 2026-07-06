308aeb8615bb79551304f7149c335c7f1f6bee1d
Azur3i <azzurey.dracai@gmail.com>
2026-07-06 22:16:11 +0200

    bugfix: added $i for real this time :3

--- scripts/functions.php
+++ scripts/functions.php
@@ -108,7 +108,7 @@ function renderAbility($ability, $z, $cls=null) {
                                 </div>
                             </div>
                         </div>
-                        $i++;
+                        <?php $i++; ?>
                     <?php endforeach; ?>
                 </div>
                 <?php break;
