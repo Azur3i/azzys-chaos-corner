0155b1661a509df9d4494eb9f1e66b40b038de05
Azur3i <azzurey.dracai@gmail.com>
2026-07-09 12:10:03 +0200

    added back to top button in footer

--- tpl/footer.php
+++ tpl/footer.php
@@ -1,6 +1,6 @@
         <footer>
             <nav class="navbar fixed-bottom" id="navbar-bottom">
-
+                <a style="height: 1.8rem; padding: 0 2rem 0 2rem;" class="button pink sm button-top mx-auto low-opac" href="#">Back to top ↑</a>
             </nav>
         </footer>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>
