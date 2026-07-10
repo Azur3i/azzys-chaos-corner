c2cf59caa8bbb51a7471b9486e81355c6d4e8fbb
Azur3i <azzurey.dracai@gmail.com>
2026-07-10 15:46:21 +0200

    moved bootstrap and jquery script elements to prevent definition errors

--- tpl/footer.php
+++ tpl/footer.php
@@ -3,8 +3,6 @@
                 <a style="height: 1.8rem; padding: 0 2rem 0 2rem;" class="button pink sm button-top mx-auto low-opac" href="#">Back to top ↑</a>
             </nav>
         </footer>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>
-        <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
         <script src="/scripts/scripts.js"></script>
     </body>
 </html>
