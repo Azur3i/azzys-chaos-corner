913fae372b8cbdc1484dbe95b74fc1a08b8dbe59
Azur3i <azzurey.dracai@gmail.com>
2026-07-10 22:22:21 +0200

    added basic mobile accomodation

--- dnd/spells.php
+++ dnd/spells.php
@@ -21,19 +21,19 @@ $targetSpell = $spells[$spellName];
 
 <div class="container-fluid">
     <div class="row justify-content-center">
-        <div class="main-content col-md-12 col-lg-9 mx-auto">
-            <div class="content-list row" style="padding: 0;">
-                <div class="content-list col-4" style="padding: 1.25rem; margin: 0;" id="spell-list">
+        <div class="main-content col-md-12 col-lg-9 mx-auto py-0">
+            <div class="content-list row" style="padding: 0;" id="spell-view">
+                <div class="content-list col-md-12 col-lg-4" style="padding: 1.25rem; margin: 0;" id="spell-list">
                     <div class="scroll">
                         <ul class="list-group">
                             <?php $i = 0;
                             foreach ($spells as $x => $y): ?>
                                 <a 
-                                    class="list-group-item blue low-opac button-list d-grid w-100 align-items-center"
+                                    class="list-group-item blue low-opac button-list d-grid w-100 align-items-center text-start"
                                     style="outline: none; box-shadow: none; grid-template-columns: 1fr auto;"
                                     id="<?= $x ?>"
                                     href="#<?= $x ?>">
-                                    <span data="name" class="md"><?= $y["name"] ?></span>
+                                    <span data="name" class="md" style="text-align: left;"><?= $y["name"] ?></span>
                                     <span data="school" class="sm" style="opacity: 0.7;"><?= get_level($y["level"], $y["school"]) ?></span>
                                 </a>
                                 <?php $i++;
@@ -41,7 +41,7 @@ $targetSpell = $spells[$spellName];
                         </ul>
                     </div>
                 </div>
-                <div id="spellbox" class="col-8" style="padding-right: 0;">
+                <div id="spellbox" class="col-md-12 col-lg-8" style="padding-right: 0;">
                     <?php include ROOT . "/scripts/construct-spell.php" ?>
                 </div>
             </div>
