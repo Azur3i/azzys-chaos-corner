a207020032db9c4c511a6138745bbec179681805
Azur3i <azzurey.dracai@gmail.com>
2026-07-29 22:00:48 +0200

    improved spell render function to include regex instead of str replace, to prevent instances like "light" and "lightning"

--- scripts/php/general.php
+++ scripts/php/general.php
@@ -39,7 +39,7 @@ function renderSpells($str) {
 
     foreach ($spells as $id => $spell) {
         $target = strtolower($spell["name"]);
-        $str = str_replace("@$target", "<i><a class='spell-link' data-spell='$id' href='/dnd/spells#$id'>$target</a></i>", $str);
+        $str = preg_replace("/@" . preg_quote($target, "/") . "\b/", "<i><a class='spell-link' data-spell='$id' href='/dnd/spells#$id'>$target</a></i>", $str);
     }
     return $str;
 }
